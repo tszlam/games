cc82555548ebec613f3227ab23832e79a6cf3918
tszlamli <tszlamli@tencent.com>
2023-05-03 11:50:42 +0800

    update

--- src/views/FlipCard.vue
+++ src/views/FlipCard.vue
@@ -1,25 +1,44 @@
 <template>
+  <input type="number" v-model="pairs"/>
+  <button @click="restart">New Game</button>
+  <div>STAET: {{ isFinished ? 'FINISHED' : 'UN FINISH' }}</div>
   <div class="board">
-    <div class="row" v-for="r in height" :key="`r_${r}`">
-      <div class="box" v-for="c in width" :key="`c_${c}`"
-        :class="{ isClicked: state.cards[(r - 1) * width + c - 1].flag }" @click="handleClickCard(r, c)">{{ state.cards[(r
-          - 1) * width + c - 1
-        ].value }}</div>
+    <div class="row" v-for="r in board.row" :key="`r_${r}`">
+      <div class="box" v-for="c in board.column" :key="`${r}_${c}`"
+        :class="{
+          isClicked: getCardNumber(r, c).state === CARD_STATE.PAIRING,
+          isPaired: getCardNumber(r, c).state === CARD_STATE.PAIRED,
+        }"
+        @click="handleClickCard(r, c)"
+      >
+        {{ getCardNumber(r, c).value }}
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { onMounted, reactive } from 'vue';
-
-const pairs = 6
+// todo: 开始时倒数，倒数是可以看数字，倒数结束后开始游戏，游戏是不可以看数字，只有激发态才可以看到数字
+import { ref, reactive, computed } from 'vue';
+const CARD_STATE = {
+  PENDING: 0,
+  PAIRING: 1,
+  PAIRED: 2,
+}
+const numPerPair = 2
+const pairs = ref(6)
 const state = reactive({
   cards: []
 })
+let pairingStack = [];
 // 这个输入对数，会算出对应个数最近似正方形的宽高边个数，宽 > 高
 // eg: 12 => [4, 3]
+/**
+ * 这个输入对数，会算出对应个数最近似正方形的行数和列数，列 > 行
+ * eg: 12 => { row: 3, column: 4 }
+ * @param {number} pairs 
+ */
 const getXYByPairs = (pairs) => {
-  // const count = pairs * 2
   const total = pairs * 2
   let width = pairs
   let res = width
@@ -29,53 +48,82 @@ const getXYByPairs = (pairs) => {
     if ((total / width) > width) break
     res = width
   }
-  return [res, total / res]
+  return {
+    row: total / res,
+    column: res,
+  }
 }
 // 在数组里随机安排n对
 const genRandomCards = (pairs) => {
   // 初始化一个有两个pairs的数组, flag用于记录是否已经被翻牌，已翻的牌再次点击不生效
-  const initNumber = Array.from({ length: pairs }).map((_, index) => { return { value: index + 1, flag: false } })
-  const cards = [...initNumber, ...initNumber];
+  const length = pairs * numPerPair
+  // value生成
+  let value = 0
+  let count = 0
+  const getValue = () => {
+      if (count === 0 ) {
+        count = numPerPair
+        value++
+      }
+      count--
+      return value
+  }
+  const cards = Array
+    .from({ length })
+    .map(() => ({ value: getValue(), flag: false, state: CARD_STATE.PENDING }));
 
   // 随机取一个数，和最后一个交换，然后改变length 重复操作
-  // const index = Math.floor(Math.random() * total)
-  let len = cards.length - 1;
+  let len = length - 1;
   while (len > 0) {
-    const target = Math.floor(Math.random() * len);
-    [cards[target], cards[len]] = [cards[len], cards[target]];
+    const target = Math.floor(Math.random() * (len + 1)); // random是[0,1) 所以要把len+1
+    if (len !== target) {
+      [cards[target], cards[len]] = [cards[len], cards[target]];
+    }
     len--;
   }
   return cards;
 }
 const getCardNumber = (row, column) => {
-  return state.cards[(row - 1) * width + column - 1];
+  return state.cards[(row - 1) * board.column + column - 1];
 }
 
-let currentRound = []; // 每两次点击，是一个round, 记录第一次点击的坐标
-// 每两次
 const handleClickCard = (row, column) => {
-  const currentNumber = getCardNumber(row, column);
-  // 如果已经翻牌了，本次点击无效
-  if (currentNumber.flag) return;
-  // 没有点击过，记录pre点击
-  if (currentRound.length === 0) {
-    currentRound = [row, column];
-  } else {
-    const pre = getCardNumber(currentRound[0], currentRound[1]);
-
-    if (pre.value === currentNumber.value) {
-      console.log('=====bingo=====');
-      pre.flag = true;
-      currentNumber.flag = true;
-    }
-    // new round
-    currentRound = [];
+  const card = getCardNumber(row, column);
+  const { value, state } = card
+  if (state !== CARD_STATE.PENDING) return;
+  if (pairingStack.length === 0) {
+    card.state = CARD_STATE.PAIRING
+    pairingStack.push(card)
+    return;
   }
+  if (pairingStack[0].value !== value) {
+    pairingStack.forEach(_card => {
+      _card.state = CARD_STATE.PENDING
+    })
+    pairingStack = []
+    return
+  }
+  if (pairingStack.length + 1 < numPerPair) {
+    card.state = CARD_STATE.PAIRING
+    pairingStack.push(card)
+  }
+  card.state = CARD_STATE.PAIRED
+  pairingStack.forEach(_card => {
+    _card.state = CARD_STATE.PAIRED
+  })
+  pairingStack = []
+}
+
+const restart = () => {
+  board = reactive(getXYByPairs(pairs.value))
+  state.cards = genRandomCards(pairs.value)
+  pairingStack = []
 }
 
 // 返回
-const [width, height] = getXYByPairs(pairs)
-state.cards = genRandomCards(pairs);
+let board = reactive(getXYByPairs(pairs.value))
+state.cards = genRandomCards(pairs.value);
+const isFinished = computed(() => state.cards.every(({ state: _s }) => _s === CARD_STATE.PAIRED ))
 
 </script>
 
@@ -93,10 +141,19 @@ state.cards = genRandomCards(pairs);
   width: 200px;
   height: 200px;
   border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bolder;
+  font-size: 40px;
 }
 
 .board .row .box.isClicked {
   background-color: red;
+  color: white;
+}.board .row .box.isPaired {
+  background-color: green;
+  color: white;
 }
 
 .board .row .box+.board .row .box {
